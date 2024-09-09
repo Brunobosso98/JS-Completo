05d314b5bc1e8bef0325ec28f8c3054608503b08
brunobossomartins1@gmail.com <brunobossomartins1@gmail.com>
2024-09-08 23:02:01 -0300

    dataset-add

---
+++ date-object/js/script.js
@@ -0,0 +1,21 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimationScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumber from "./modules/anima-number.js";
+import initFuncionamento from "./modules/funcionamento.js";
+
+initScrollSuave();
+initAnimationScroll();
+initAccordion();
+initTabNav();
+initModal();
+initToolTip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumber();
+initFuncionamento();
