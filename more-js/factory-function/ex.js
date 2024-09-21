4f766aeea9052fed7bcc0289f8a92f42f66e86dd
brunobossomartins1@gmail.com <brunobossomartins1@gmail.com>
2024-09-21 15:11:23 -0300

    closures-and-debugging

--- more-js/factory-function/ex.js
+++ more-js/factory-function/ex.js
@@ -1,6 +1,8 @@
+// Função para selecionar
 function $$(selectedElements) {
   const elements = document.querySelectorAll(selectedElements);
 
+  // Função para esconder os botões
   function hide() {
     elements.forEach((element) => {
       element.style.display = "none";
@@ -8,6 +10,7 @@ function $$(selectedElements) {
     return this;
   }
 
+  // Função para mostrar os botões
   function show() {
     elements.forEach((element) => {
       element.style.display = "initial";
@@ -15,6 +18,7 @@ function $$(selectedElements) {
     return this;
   }
 
+  // Função para adicionar um eventListener
   function on(onEvent, callback) {
     elements.forEach((element) => {
       element.addEventListener(onEvent, callback);
@@ -22,6 +26,7 @@ function $$(selectedElements) {
     return this;
   }
 
+  // Função para adicionar uma classe
   function addClass(className) {
     elements.forEach((element) => {
       element.classList.add(className);
@@ -29,6 +34,7 @@ function $$(selectedElements) {
     return this;
   }
 
+  // Função para remover uma classe
   function removeClass(className) {
     elements.forEach((element) => {
       element.classList.remove(className);
@@ -46,13 +52,16 @@ function $$(selectedElements) {
   };
 }
 
+//  Diz para a função de selecionar para selecionar (button)
 const btns = $$("button");
 
 console.log(btns.hide().show());
 
+// Faz algo quando a função ON ativar
 function handleClick(event) {
   console.log(event.target);
   btns.addClass("active");
 }
 
+// Adiciona o evento de CLICK na função de eventListener
 btns.on("click", handleClick);
