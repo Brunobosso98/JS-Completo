7d94191915cf6fbfeb31d3f898c7c1bc551d4411
brunobossomartins1@gmail.com <brunobossomartins1@gmail.com>
2024-10-14 20:58:24 -0300

    babel

---
+++ Automação/Babel/js/script.js
@@ -0,0 +1,30 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimationScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBtc from "./modules/fetch-btc.js";
+
+initScrollSuave();
+initAnimationScroll();
+initAccordion();
+initTabNav();
+initModal();
+initToolTip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBtc();
+
+import $ from "jquery";
+import _ from "lodash";
+
+// $("nav").hide();
+// const diferenca = _.difference(["Banana", "Uva"], ["Banana", "Morango"]);
+// console.log(diferenca);
